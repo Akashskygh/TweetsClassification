adc43fe71847093774cda26b1812275e7d708065
Akash Ghosh <akashghosh1998@yahoo.com>
2023-10-31 13:42:24 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
-
 # storing API data
-api_key = "0dLY3MtxYdEnNlWsZTd838CQb"
-api_secret = "2GhdEpisB3kKLzeXwIQt1hubfA5JZVRwyrQJqjZWHA4aeUmZDy"
-bearer_token = "AAAAAAAAAAAAAAAAAAAAACfkkQEAAAAAxQsLnYaF61IR5f%2FlpaiiYUB1aXo%3DG4is15sYaOMJpvg6SnZqqo80SjEd9P2dFTo6V9mUnjHm5C60OP"
-access_token = "1603175462897078273-iUjSNASwNEwo6fO3lnojbf2W4vCrI5"
-token_secret = "NDZz9p1N8VjHF9mGsI30FndvsaksZctFVfd2TDvVZzV54"
+api_key = "key"
+api_secret = "secretKey"
+bearer_token = "bearerToken"
+access_token = "accessToken"
+token_secret = "secretToken"
